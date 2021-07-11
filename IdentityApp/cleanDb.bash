cfbfad337c707d2c606893a77ed5043493a7fe65
edwardhsueh <edward.hsueh@gmail.com>
2021-07-11 12:47:33 +0800

    support ubuntu evn

---
+++ IdentityApp/cleanDb.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+dotnet ef database drop --force --context ProductDbContext
+dotnet ef database drop --force --context IdentityAppIdentityDbContext
+dotnet ef database update --context ProductDbContext 
+dotnet ef database update --context IdentityAppIdentityDbContext 
